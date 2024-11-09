b3634bff642c901669135af24669b07b94cf2aa8
sadhef <142220210+sadhef@users.noreply.github.com>
2024-11-09 16:58:48 +0530

    Delete server directory

--- server/routes/owner/analytics.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getRevenueAnalytics } from '../../controllers/owner/analytics.controller.js';
-import verifyOwnerToken from '../../middleware/jwt/owner.middleware.js';
-
-const router = Router();
-
-router.get('/revenue', verifyOwnerToken, getRevenueAnalytics);
-
-export default router;
