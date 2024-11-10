362a642a98123a8f3dd70078aa0b43ac3ccfcbf3
sadhef <142220210+sadhef@users.noreply.github.com>
2024-11-10 13:52:06 +0530

    Delete server directory

--- server/models/timeSlot.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const timeSlotSchema = new mongoose.Schema({
-    turf:{ type:mongoose.Schema.Types.ObjectId, ref:'Turf'},
-    startTime: {type:Date, required:true},
-    endTime: {type:Date, required:true},
- }, { timestamps: true });
-
-export default mongoose.model("TimeSlot", timeSlotSchema);
